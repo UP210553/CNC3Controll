dc69a816da56d7c0e06b2795f6e823ee6ea60dd8
UP210553 <up210553@alumnos.upa.edu.mx>
2024-07-19 08:44:44 -0600

    Initial commit

---
+++ CNC3Cont485/Form1.cs
@@ -0,0 +1,15 @@
+namespace CNC3Cont485
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void tbMacros_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
